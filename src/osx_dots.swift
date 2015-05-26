7f8050a6267c4182de1684bd3ddb6e624779d6bb
David Yu <lewen.yu@gmail.com>
2015-05-25 23:59:50 -0700

    OSX build no longer crashing!

--- src/osx_dots.swift
+++ src/osx_dots.swift
@@ -1,10 +1,15 @@
 import AppKit
 
 func main(args: [String]) -> Int {
+    var app: NSApplication = NSApplication.sharedApplication()
+
     var window: NSWindow = NSWindow( contentRect : NSMakeRect( 0, 0, 100, 100 )
                                    , styleMask   : NSBorderlessWindowMask
-                                   , backing     : .Buffered
-                                   , defer       : false )
+                                   , backing     : NSBackingStoreType.Buffered
+                                   , defer       : true )
+
+    window.makeKeyAndOrderFront( window )
+
     return 0
 }
 
