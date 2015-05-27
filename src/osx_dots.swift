dc763f77b61e28a1f66102c7cb59ae12462e11c9
David Yu <lewen.yu@gmail.com>
2015-05-26 23:24:23 -0700

    launch window in OS X!

--- src/osx_dots.swift
+++ src/osx_dots.swift
@@ -1,14 +1,42 @@
 import AppKit
 
+class WindowDelegate: NSObject, NSWindowDelegate {
+    func windowWillClose( notification: NSNotification ) {
+        NSApplication.sharedApplication().terminate(0)
+    }
+}
+
+class ApplicationDelegate: NSObject, NSApplicationDelegate {
+    var window: NSWindow
+
+    init( window: NSWindow ) {
+        self.window = window
+    }
+
+    func applicationDidFinishLaunching( notification: NSNotification ) {
+    }
+}
+
 func main(args: [String]) -> Int {
     var app: NSApplication = NSApplication.sharedApplication()
+    app.setActivationPolicy( NSApplicationActivationPolicy.Regular ) // this tells OS X that this is a standard application that appears in the dock
 
     var window: NSWindow = NSWindow( contentRect : NSMakeRect( 0, 0, 100, 100 )
                                    , styleMask   : NSBorderlessWindowMask
                                    , backing     : NSBackingStoreType.Buffered
                                    , defer       : true )
 
+    window.center()
+    window.title = "Dots"
     window.makeKeyAndOrderFront( window )
+
+    let windDelegate = WindowDelegate()
+    window.delegate = windDelegate
+
+    let appDelegate = ApplicationDelegate( window: window )
+    app.delegate = appDelegate
+    app.activateIgnoringOtherApps( true )
+    app.run()
 
     return 0
 }
