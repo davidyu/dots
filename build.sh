1f6500296f2e2488376d6009ca0f6b8f419de239
David Yu <lewen.yu@gmail.com>
2015-06-26 11:33:24 -0700

    OSX vector shims

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ BIN=build
 
 mkdir -p $BIN
 cd $BIN
-xcrun -sdk macosx swiftc ../src/osx_dots.swift
+xcrun -sdk macosx swiftc ../src/osx/osx_dots.swift ../src/osx/shims/vec.swift
---
+++ src/osx/shims/vec.swift
@@ -0,0 +1,11 @@
+struct Vec4 {
+    var v = [Float]( count: 4, repeatedValue: 0.0 )
+}
+
+struct Vec3 {
+    var v = [Float]( count: 3, repeatedValue: 0.0 )
+}
+
+struct Vec2 {
+    var v = [Float]( count: 2, repeatedValue: 0.0 )
+}
