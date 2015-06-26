795abc46ccbfee57a19e37501aa4450c47ec787d
David Yu <lewen.yu@gmail.com>
2015-06-26 14:14:58 -0700

    type-level constraints for OSX shim vectors
    - because no C++-like templates, this is still reasonable as a
      constraint

---
+++ src/osx/shims/nat.swift
@@ -0,0 +1,18 @@
+// use the type system for peano arithmetic
+protocol Nat {}
+
+class Zero:Nat {}
+
+// successor operator
+class S<T:Nat>:Nat {}
+
+typealias One   = S<Zero>;
+typealias Two   = S<One>;
+typealias Three = S<Two>;
+typealias Four  = S<Three>;
+typealias Five  = S<Four>;
+typealias Six   = S<Five>;
+typealias Seven = S<Six>;
+typealias Eight = S<Seven>;
+typealias Nine  = S<Eight>;
+typealias Ten   = S<Nine>;
--- src/osx/shims/vec.swift
+++ src/osx/shims/vec.swift
@@ -1,11 +1,20 @@
-struct Vec4 {
+// unsafe base
+struct Vec<T:Nat> {
+    var v:[Float]
+}
+
+struct Vec<Four> {
     var v = [Float]( count: 4, repeatedValue: 0.0 )
 }
 
-struct Vec3 {
+struct Vec<Three> {
     var v = [Float]( count: 3, repeatedValue: 0.0 )
 }
 
-struct Vec2 {
+struct Vec<Two> {
     var v = [Float]( count: 2, repeatedValue: 0.0 )
 }
+
+typealias Vec4 = Vec<Four>
+typealias Vec3 = Vec<Three>
+typealias Vec2 = Vec<Two>
