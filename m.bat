00af906c37fa97bd3717bc687716deba29d243f8
David Yu <lewen.yu@gmail.com>
2016-06-21 20:06:45 -0700

    clean up win32 build script
    move build flags into its own file

--- m.bat
+++ m.bat
@@ -1,5 +1,5 @@
 @echo off
 @if not exist build mkdir build
 pushd build
-cl -Zi ..\src\win32\lib\gml-cpp\src\mat.cpp ..\src\win32\win32_dots.cpp ..\src\win32\shims\camera.cpp ..\src\win32\shims\frustum.cpp ..\src\win32\rasterizer.cpp user32.lib Gdi32.lib -I..\src\win32\shims -I..\src\win32\lib\gml-cpp\src -I..\src\win32\ -FI types.inc 
+cl @..\vcbuildflags
 popd
