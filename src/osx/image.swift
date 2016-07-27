36756172773ca0541612972ed66693b4e32a2a16
David Yu <lewen.yu@gmail.com>
2016-07-26 23:58:41 -0700

    fix NSImage construction

--- src/osx/image.swift
+++ src/osx/image.swift
@@ -8,20 +8,21 @@ public struct Pixel {
     var b:UInt8
 }
 
-public func imageFromBitmap( pixels: [Pixel], width: UInt, height:UInt ) -> NSImage {
-    let provider = CGDataProviderCreateWithCFData( NSData( bytes: &pixels, length: pixels.count * sizeof( Pixel ) ) )
+public func imageFromBitmap( pixels: [Pixel], width: Int, height:Int ) -> NSImage {
+    var data = pixels
+    let provider = CGDataProviderCreateWithCFData( NSData( bytes: &data, length: pixels.count * sizeof( Pixel ) ) )
 
     let img = CGImageCreate( width,
                              height,
                              8,  // bits per component
                              32, // bits per pixel
-                             width * UInt( sizeof( Pixel ) ),
+                             width * Int( sizeof( Pixel ) ),
                              CGColorSpaceCreateDeviceRGB(),
-                             CGBitmapInfo( CGImageAlphaInfo.PremultipliedFirst.toRaw() ),
+                             CGBitmapInfo( rawValue: CGImageAlphaInfo.PremultipliedFirst.rawValue ),
                              provider,
                              nil,
                              true,
                              CGColorRenderingIntent.RenderingIntentDefault )
 
-    return NSImage( CGImage: img )
+    return NSImage( CGImage: img!, size: CGSize( width: width, height: height ) )
 }
